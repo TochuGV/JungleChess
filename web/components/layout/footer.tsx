30e90677df4c16e96cdc5131e3b29b4e4c141f22
Mateo Ziffer <mateoziffer@gmail.com>
2023-02-19 15:50:28 -0300

    5 - Footer Add more social media links
    Co-authored-by: Tobías Vallejos <TochuGV@users.noreply.github.com>

--- web/components/layout/footer.tsx
+++ web/components/layout/footer.tsx
@@ -9,7 +9,16 @@ const Footer: FC = () => {
       <div>
         <p>Jungle Chess &#169; 2023</p>
         <Link href="https://discord.gg/bJbRaCrJ3x" target="_blank" rel="noopener noreferrer">
-          <i className="discord-icon" />
+          <i className="fa-brands fa-discord text-[#5865F2] text-2xl" />
+        </Link>
+        <Link href="https://twitter.com/JungleChess" target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-twitter text-[#00ACEE] text-2xl" />
+        </Link>
+        <Link href="https://www.instagram.com/junglechess/" target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-instagram text-[#ce1efa] text-2xl" />
+        </Link>
+        <Link href="https://github.com/TochuGV/JungleChess" target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-github text-[#fafafa] text-2xl" />
         </Link>
       </div>
     </footer>
