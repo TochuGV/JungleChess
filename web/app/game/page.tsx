03632d5cfa9d06771d155d35b765888a1914c139
Mateo Ziffer <mateoziffer@gmail.com>
2025-03-29 15:39:15 -0300

    8 - BUG FIX - Solve Merge Error

--- web/app/game/page.tsx
+++ web/app/game/page.tsx
@@ -12,7 +12,7 @@ import whoWon from "@/helpers/game/whoWon";
 import useCellSize from "@/hooks/useCellSize";
 import { Board, BoardPosition, PieceType } from "@/types/game";
 import Image from "next/image";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 interface PieceProps {
   piece: string;
@@ -239,33 +239,33 @@ export default function Page() {
           marginBottom: margin.y / 2,
         }}>
         {new Array(board.height).fill(null).map((_, y) => (
-          new Array(board.width).fill(null).map((_, x) => (
-						const isFirstRow = y === board.height - 1;
-						const isFirstColumn = x === 0;
-						return (
-							<div
-								key={`${x} - ${y}`}
-								className={`bg-primary-${getCellColor(x, y)}`}
-							>
-								{isFirstRow && (
-									<span
-										className="absolute text-white font-semibold z-10"
-										style={{transform: `translate(${cellSize - 16}px, ${cellSize - 20}px)`}}
-									>
-										{String.fromCharCode(65 + x)}
-									</span>
-								)}
-								{isFirstColumn && (
-									<span
-										className="absolute text-white font-semibold z-10"
-										style={{transform: `translate(4px, 2px)`}}
-									>
-										{board.height - y}
-									</span>
-								)}
-          ))
+          new Array(board.width).fill(null).map((_, x) => {
+            const isFirstRow = y === board.height - 1;
+            const isFirstColumn = x === 0;
+            return (
+              <div
+                key={`${x} - ${y}`}
+                className={`bg-primary-${getCellColor(x, y)}`}
+              >
+              {isFirstRow && (
+                  <span
+                    className="absolute text-white font-semibold z-10"
+                    style={{transform: `translate(${cellSize - 16}px, ${cellSize - 20}px)`}}
+                  >
+                      {String.fromCharCode(65 + x)}
+                  </span>
+                  )}
+              {isFirstColumn && (
+                <span
+                  className="absolute text-white font-semibold z-10"
+                  style={{transform: `translate(4px, 2px)`}}
+                >
+                  {board.height - y}
+                </span>
+              )}
+            </div>)
+          })
         ))}
-
         {board.objects.traps.map(trap =>
           <Trap
             position={trap.position}
